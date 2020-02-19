72f8e4342dc6ad1722e808d4234699bed2d43421
romncn <romncn@gmail.com>
2020-02-20 01:00:19 +0700

    [ADD]create Folder Component{Header.js,Content.js,Footer.js}

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Footer from './Components/Footer';
 
 class App extends React.Component {
   render() {
@@ -13,37 +16,9 @@ class App extends React.Component {
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-          <h1>Welcome to Basic React By Romncn</h1>
-      </div>
-    );
-  }
-}
 
 
-class Content extends React.Component {
-  render() {
-    return (
-      <div>
-            <h2>React Content</h2>
-      </div>
-    );
-  }
-}
 
-
-class Footer extends React.Component {
-  render() {
-    return (
-      <div>
-          <h3>Thank you for coding ♥</h3>
-      </div>
-    );
-  }
-}
 
 
 export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+      return (
+        <div>
+            <h3>Thank you for coding ♥</h3>
+        </div>
+      );
+    }
+  }
+
+  export default Footer;
