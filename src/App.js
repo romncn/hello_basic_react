231bd1d65d1df7999d9e4f2de49338f4cfb7af38
romncn <romncn@gmail.com>
2020-02-20 00:49:57 +0700

    [UPDATE]App.js create component Header,Content,Footer

--- src/App.js
+++ src/App.js
@@ -5,10 +5,45 @@ class App extends React.Component {
   render() {
     return (
       <div>
-          <h1>Basic React</h1>
+          <Header></Header>
+          <Content></Content>
+          <Footer></Footer>
       </div>
-    )
+    );
   }
 }
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+          <h1>Welcome to Basic React By Romncn</h1>
+      </div>
+    );
+  }
+}
+
+
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+            <h2>React Content</h2>
+      </div>
+    );
+  }
+}
+
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div>
+          <h3>Thank you for coding ♥</h3>
+      </div>
+    );
+  }
+}
+
 
 export default App;
